d814e6a653075f558092ab11cdeb827239fa513f
rahul300302 <114736219+rahul300302@users.noreply.github.com>
2025-03-07 19:43:09 +0530

    Delete backend directory

--- backend/middleware/utils/generateToken.js
+++
@@ -1,6 +0,0 @@
-const jwt=require("jsonwebtoken")
-
-module.exports.generateToken = async(id) => {
-    console.log(id);
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
-};
--- backend/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const Router = express.Router();
-
-const Admin = require("./Admin/router"); 
-
-Router.use("/api", Admin); 
-
-module.exports = Router; 
